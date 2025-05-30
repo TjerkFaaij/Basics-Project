aa505a7e984c4355114dcd8aa481aef9dceae097
Tjerk Faaij <tjerk.faay@gmail.com>
2025-05-30 11:32:12 +0200

    Update RecipeListPage

--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -1,47 +1,39 @@
-import { Center, Text } from '@chakra-ui/react';
-import { data } from '../utils/data';
+import { Center, Text } from "@chakra-ui/react";
+import { data } from "../utils/data";
 import { SimpleGrid } from "@chakra-ui/react";
-import { RecipeCard } from '../components/ui/RecipeCard';
-import { useState } from 'react';
+import { RecipeCard } from "../components/ui/RecipeCard";
+import { useState } from "react";
 import { TextInput } from "../components/ui/TextInput";
 
-//need to add ids to the data file
-
-export const RecipeListPage = ({selectRecipe}) => {
-  // You can play around with the console log, but ultimately remove it once you are done
-  //console.log(data.hits[0].recipe.id);
-  
-  const [searchField, setSearchField] = useState();
+export const RecipeListPage = ({ selectRecipe }) => {
+  const [searchField, setSearchField] = useState("");
 
   const handleChange = (event) => {
-    setSearchField(event.target.value);
+    setSearchField(event.target.value.toLowerCase());
   };
 
-  const matchedRecipes = data.hits.filter((recipe) => {
-    return recipe.recipe.label.toLowerCase().includes(searchField);
-  });
-
-  console.log(matchedRecipes);
+  const matchedRecipes = data.hits.filter((recipe) =>
+    recipe.recipe.label.toLowerCase().includes(searchField)
+  );
 
   return (
     <>
-    <Text mt={4} fontSize={"3xl"}>Search for recipes:</Text>
-    <TextInput changeFn={handleChange} w={[300, 380]} mb={8} />
+      <Text mt={4} fontSize="3xl">
+        Search for recipes:
+      </Text>
+      <TextInput changeFn={handleChange} w={[300, 380]} mb={8} />
 
-    <Center flexDir="column">
-      <SimpleGrid columns={[1, 1, 2, 3, 3, 4]} spacing={10}>
-        { matchedRecipes.map((recipe) => (
-        //  console.log(recipe.recipe.id)
-           <RecipeCard
-            key={recipe.recipe.id}
-            recipe = {recipe.recipe}
-            selectRecipe={selectRecipe}
-          />
-        ))}
-      </SimpleGrid>
-
-    </Center>
+      <Center flexDir="column">
+        <SimpleGrid columns={[1, 1, 2, 3, 3, 4]} spacing={10}>
+          {matchedRecipes.map((recipe) => (
+            <RecipeCard
+              key={recipe.recipe.id}
+              recipe={recipe.recipe}
+              selectRecipe={selectRecipe}
+            />
+          ))}
+        </SimpleGrid>
+      </Center>
     </>
   );
 };
-
